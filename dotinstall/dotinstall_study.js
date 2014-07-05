c8ab57b5d6e8720213db5b94dcee315d510911d9
haya14busa <hayabusa1419@gmail.com>
2014-07-06 01:33:06 +0900

    SVG!

--- dotinstall/dotinstall_study.js
+++ dotinstall/dotinstall_study.js
@@ -37,4 +37,26 @@
 
     exit.style('color', 'red'); //.remove
 
+    /*
+     * SVG
+     */
+    var dataset = [11, 25, 45, 30, 33]; // radius
+
+    var w = 500;
+    var h = 200;
+
+    var svg = d3.select('body').append('svg').attr({width: w, height: h});
+
+    svg.selectAll('circle')
+       .data(dataset) // data biding
+       .enter()
+       .append('circle')
+       .attr({
+           cx: function(d, i) { return  50 + (i * 100); },
+           cy: h / 2,
+           r: function(d) { return d;},
+           fill: 'red'
+       });
+
+
 })();
